84e74706e7adcf416ec728906aacefe196852f9f
cynddl <luc@rocher.lc>
2015-06-30 17:29:25 -0400

    Update the legend of the network view

--- demo/viz/network_view/networkview.js
+++ demo/viz/network_view/networkview.js
@@ -4,39 +4,8 @@ var mark_offset = 10; // how many percent of the mark lines in each end are not 
 var mark_size = 7; // size of the mark on the line
 var line_width_factor = 10.0 // width for the widest line
 
-var legendRectSize = 9; // 18
-var legendSpacing = 5; // 4
-var recordTypes = [];
-var legend;
-var legendDrawn = false;
-
 var text_links_data, call_links_data;
 
-// colors for the different parts of the visualization
-recordTypes.push({
-	text : "call",
-	color : "#438DCA",
-	legendtype : "legend_rect"
-});
-
-recordTypes.push({
-	text : "text",
-	color : "#70C05A",
-	legendtype : "legend_rect"
-});
-
-
-recordTypes.push({
-	text : "in network",
-	color : "#a8a8a8",
-	legendtype : "legend_circle"
-});
-
-recordTypes.push({
-	text : "out of network",
-	color : "#dfdfdf",
-	legendtype : "legend_circle"
-});
 
 // Function for grabbing a specific property from an array
 pluck = function (ary, prop) {
@@ -221,10 +190,6 @@ function tick(e) {
 		});
 
 	}
-
-	if (force.alpha() < 0.05 && legendDrawn == false)
-		drawLegend();
-
 }
 
 function applyGradient(line, interaction_type, d, i) {
@@ -457,8 +422,6 @@ function CreateVisualizationFromData() {
 		.style("stroke", function (d) {
 			return (d.index == 0) ? "#8C8C8C" : "#ffffff";
 		})
-		.on("mouseover", mouseOverFunction)
-		.on("mouseout", mouseOutFunction);
 
 	//EXIT
 	node.exit().remove;
@@ -491,15 +454,13 @@ function CreateVisualizationFromData() {
 		.append("path")
 		.attr("d", "M 0,0 V 4 L6,2 Z");
 
-	$('.node').tipsy({
-		gravity: 'w',
-	html: true,
-	title: function() {
-	var d = this.__data__, name = d.name;
-	return name;
-	//var d = this.__data__, id = d.id, source_calls = d.source_calls, target_calls = d.target_calls, source_texts = d.source_texts, target_texts = d.target_texts;
-	//return 'Incoming Calls: ' + source_calls + '<br/>' + 'Outgoing Calls: ' + target_calls + '<br/>' + 'Incoming Texts: ' + source_texts + '<br/>' + 'Outgoing Texts: ' + target_texts ;
-	}
+		$('.node').tipsy({
+			gravity: 'w',
+			html: true,
+			title: function() {
+			var d = this.__data__, name = d.name;
+			return name;
+		}
 	});
 		
 	if (text_links_data.length > 0) {
@@ -528,228 +489,6 @@ function CreateVisualizationFromData() {
 	.on("tick", tick);
 
 }
-
-function drawLegend() {
-
-	legendDrawn = true;
-	var node_px = pluck(data_nodes, 'px');
-	var node_py = pluck(data_nodes, 'py');
-	var nodeLayoutRight = Math.max(maxArray(node_px)) + 115;
-	var n_horisontal_text = 4;
-	//var nodeLayoutBottom = (Math.max(maxArray(node_py)) + Math.min(minArray(node_py))) * 0.5 + 0.5 * (legendRectSize + legendSpacing) * (n_horisontal_text - 1);
-	var total_size = (legendRectSize + legendSpacing) * (n_horisontal_text - 1);
-	var nodeLayoutBottom = node_py[0]  - total_size/2;
-	var legend_next_section_vert_offset = 120;
-	var legend_line_width = 40;
-
-	legend = svg.selectAll('.legend')
-		.data(recordTypes)
-		.enter()
-		.append('g')
-		.attr('class', 'legend')
-		.attr('class', function (d) {
-			return d.legendtype
-		})
-		.attr('transform', function (d, i) {
-			//var offset = rect_height * (recordTypes.length - 1);
-			var horz = nodeLayoutRight;
-			/*  - 2*legendRectSize; */
-			//var vert = nodeLayoutBottom + (i * (rect_height)) - offset;
-			var vert = nodeLayoutBottom + (i * (legendRectSize + legendSpacing));
-			return 'translate(' + horz + ',' + vert + ')';
-		});
-
-	svg.selectAll('.legend_rect')
-	.append('rect')
-	.attr('width', legendRectSize)
-	.attr('height', legendRectSize)
-	.style('fill', function (d) {
-		return d.color
-	})
-	.style('stroke', function (d) {
-		return d.color
-	});
-
-	svg.selectAll('.legend_circle')
-	.append('circle')
-	.attr('r', legendRectSize / 2)
-	.style('fill', function (d) {
-		return d.color
-	})
-	.style('stroke', function (d) {
-		return d.color
-	})
-	.attr('transform', 'translate(' + legendRectSize / 2 + ',' + (legendRectSize / 2 + 1.1) + ')');
-
-	legend.append('text')
-	.attr('x', legendRectSize + legendSpacing)
-	.attr('y', legendRectSize - legendSpacing + 5)
-	.text(function (d) {
-		return d.text;
-	})
-	.style('fill', '#757575');
-
-	//
-	
-
-	
-	var size_link_data = [{
-			percentage : 0.5,
-			roundoff : 2,
-			recordType: 0,
-			class: "text-line",
-		}, {
-			percentage : 0.3,
-			roundoff : 1,
-			recordType: 0,
-			class: "text-line",
-		},
-		{
-			percentage : 0.5,
-			roundoff : 2,
-			recordType: 1,
-			class: "call-line",
-			text: "",
-			source_texts: 6,
-			target_texts: 2,
-			x1: 0,
-			y1: 0,
-			x2: legend_line_width,
-			y2: 0,
-			source: {index: 999, px: 0, py: 0},
-			target: {index: 999, px: legend_line_width, py: 0}
-		}
-	]
-	
-	
-	
-	size_link_data.forEach(function (d,i) {
-		d.interaction_size = Math.round(max_interactions*d.percentage/10)*10;
-		d.width = d.interaction_size / max_interactions * line_width_factor;
-		
-	})
-	
-	
-	var legend_next_section = svg.selectAll('.legend_next_section')
-	.data([size_link_data[0],size_link_data[1]])
-	.enter()
-	.append('g')
-	.attr('class', 'legend')
-	.attr('class', function (d) { return d.class })
-	.attr('transform', 'translate(' + (nodeLayoutRight+legend_next_section_vert_offset)  + ',' + (nodeLayoutBottom) + ')');
-		
-	/* sizeLink.enter().append("line") */
-	sizeLines = legend_next_section.append("line")
-		.style('stroke', function (d) {
-		    return recordTypes[d.recordType].color
-		})
-		.attr("x1", function (d, i) {
-			return 0;
-		})
-		.attr("x2", function (d, i) {
-			return legend_line_width;
-		})
-		.attr("y1", function (d, i) {
-			return (i * (legendRectSize + legendSpacing)) + legendRectSize/2; //+ d.width/4;
-		})
-		.attr("y2", function (d, i) {
-			return (i * (legendRectSize + legendSpacing))  + legendRectSize/2; //+ d.width/4;
-		})
-		.style("stroke-width", function (d, i) {
-			return d.width;
-		});
-		
-	legend_next_section.append('text')
-	.attr('x', 45)
-	.attr('y', function (d, i) {
-			return (i * (legendRectSize + legendSpacing) + legendRectSize - legendSpacing + 5);
-		})
-	.text(function (d) {
-		return typeof(d.text) !== "undefined" ? d.text : d.interaction_size;
-	})
-	.style('fill', '#757575');
-	
-
-	// create balance line and apply gradient
-	balanceLine = svg.selectAll(".balance-line")
-		.data([size_link_data[2]])
-		.enter().append("line")
-		.attr('class', 'call-line')
-		.attr('class', 'legend')
-		.attr("x1", function (d, i) {
-			return 0;
-		})
-		.attr("x2", function (d, i) {
-			return legend_line_width;
-		})
-		.attr("y1", (3 * (legendRectSize + legendSpacing))  + legendRectSize/2)
-		.attr("y2", (3 * (legendRectSize + legendSpacing))  + legendRectSize/2)
-		.style("stroke-width", function (d, i) {
-			return d.width;
-		})
-		.style("stroke", "black")
-		.attr('transform', 'translate(' + (nodeLayoutRight+legend_next_section_vert_offset)  + ',' + (nodeLayoutBottom) + ')');
-	
-	balanceLine
-	.each(function (d, i) {
-			applyGradient(this, "text", d, i)
-		});
-
-	// create separate section for text/circles around balance to avoid gradient issues
-	var balance_section = svg.selectAll('.legend_balance_section')
-	.data([size_link_data[2]])
-	.enter()
-	.append('g')
-	.attr('class', 'legend')
-	.attr('class', function (d) { return d.class })
-	.attr('transform', 'translate(' + (nodeLayoutRight+legend_next_section_vert_offset)  + ',' + (nodeLayoutBottom) + ')');
-	
-	balance_section.append('text')
-	.attr('x', 45)
-    .attr('y', (3 * (legendRectSize + legendSpacing) + legendRectSize - legendSpacing + 5))
-	.text(function (d) {
-		return "a texts b the most";
-	})
-	.style('fill', '#757575');
-	
-	balance_section.append('circle')
-    .attr('r', legendRectSize*0.5)
-	.style('fill', function (d) {
-		return recordTypes[3].color
-	})
-	.style('stroke', function (d) {
-		return recordTypes[3].color
-	})
-	.attr('transform', 'translate(' + legendRectSize/2 + ',' + (total_size+legendRectSize / 2) + ')')
-	
-	balance_section.append('circle')
-    .attr('r', legendRectSize*0.5)
-	.style('fill', function (d) {
-		return recordTypes[3].color
-	})
-	.style('stroke', function (d) {
-		return recordTypes[3].color
-	})
-	.attr('transform', 'translate(' + (legend_line_width - legendRectSize/2) + ',' + (total_size+legendRectSize / 2) + ')')
-	
-	balance_section.append('text')
-	.attr('x', 0)
-    .attr('y',  (2 * (legendRectSize + legendSpacing) + legendRectSize - legendSpacing + 5))
-	.text('a')
-	.style('fill', '#757575')
-	.style("font-weight", "bold")
-	
-	balance_section.append('text')
-	.attr('x', legend_line_width-legendRectSize)
-    .attr('y',  (2 * (legendRectSize + legendSpacing) + legendRectSize - legendSpacing + 5))
-	.text('b')
-	.style('fill', '#757575')
-	.style("font-weight", "bold")
-	
-
-	
-}
-
 
 
 function call_width(d) {
@@ -796,40 +535,4 @@ function getAngle(d) {
 	rel_x = d.target.x - d.source.x;
 	rel_y = d.target.y - d.source.y;
 	return theta = Math.atan2(rel_y, rel_x);
-}
-
-var mouseOverFunction = function (d) {
-	var circle = d3.select(this);
-
-	/* node
-	.transition(500)
-	.style("opacity", function(o) {
-	return isConnected(o, d) ? 1.0 : 0.2 ;
-	})
-
-	/* link
-	.transition(500)
-	.style("stroke-opacity", function(o) {
-	return o.source === d || o.target === d ? 1 : 0.3;
-	});
-
-	circle
-	.transition(500)
-	.attr("r", function(){ return 1.4 * node_radius(d)}); */
-
-}
-
-var mouseOutFunction = function () {
-	var circle = d3.select(this);
-
-	/* node
-	.transition(500); */
-
-	/* link
-	.transition(500)
-	.style("stroke-opacity", 1);
-
-	circle
-	.transition(500)
-	.attr("r", node_radius);*/
 }
