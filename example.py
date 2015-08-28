719584be6737dc26ebac42d819503ea599649498
Bjarke <bjarke@felbo.dk>
2015-08-28 15:57:40 +0200

    Remove example uploaded by mistake

--- example.py
+++
@@ -1,8 +0,0 @@
-
-import bandicoot as bc
-
-
-u = bc.read_csv('X', 'bandicoot/tests/samples/special', attributes_path='bandicoot/tests/')
-indicators = bc.utils.all(u)
-#print indicators
-#print indicators['number_of_weeks']
