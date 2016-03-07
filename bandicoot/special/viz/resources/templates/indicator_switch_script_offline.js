672cc00861af64d6ab891ec26ec0ccfc27f1e1fa
cynddl <luc@rocher.lc>
2016-03-07 13:56:07 +0100

    Cleaning visualization

--- bandicoot/special/viz/resources/templates/indicator_switch_script_offline.js
+++
@@ -1,4 +0,0 @@
-      function switch_indicator(s){
-         var out = "<img src='stuff/indicators/".concat(s).concat(".svg' class='maximum_size'/>")
-         document.getElementById("indicator_box").innerHTML = out
-      }
